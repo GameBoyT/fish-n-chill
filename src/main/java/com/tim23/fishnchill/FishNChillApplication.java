734adb8b27253fcf3e199ee8983cdb8ec28a5f1a
gmb <gameboyt7@gmail.com>
2021-10-28 16:01:47 +0200

    init

---
+++ src/main/java/com/tim23/fishnchill/FishNChillApplication.java
@@ -0,0 +1,13 @@
+package com.tim23.fishnchill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishNChillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishNChillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tim23/fishnchill/FishNChillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim23.fishnchill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishNChillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
