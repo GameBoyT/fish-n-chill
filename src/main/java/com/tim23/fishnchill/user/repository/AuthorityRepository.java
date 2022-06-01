88be04d0f3a3e3815a49422c73045822a25518de
nemanjamarkovic <nemanja_markovic1998@hotmail.com>
2022-06-01 11:38:26 +0200

    owner request confirmation

--- src/main/java/com/tim23/fishnchill/user/repository/AuthorityRepository.java
+++ src/main/java/com/tim23/fishnchill/user/repository/AuthorityRepository.java
@@ -3,6 +3,17 @@ package com.tim23.fishnchill.user.repository;
 import com.tim23.fishnchill.user.model.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
     Authority findByName(String name);
+
+    @Override
+    List<Authority> findAll();
+
+    List<Authority> findByNameContaining(String name);
+
+    Authority getById(Long id);
+
 }
